ae5b7c874e4e4711db67bf0b498fb372d130a737
‘Lekky <lekkyjay@yahoo.com>
2024-05-11 16:20:01 +0200

    Updated pg config.

--- src/dbConn/index.ts
+++ src/dbConn/index.ts
@@ -1,6 +1,8 @@
 import { Pool, Client } from 'pg'
 
-const pool = new Pool({ ssl: { rejectUnauthorized: false } })
-const client = new Client({ ssl: { rejectUnauthorized: false } })
+const ssl = process.env.NODE_ENV === 'production' ? true : { rejectUnauthorized: false }
+
+const pool = new Pool({ ssl })
+const client = new Client({ ssl })
 
 export { pool, client }
